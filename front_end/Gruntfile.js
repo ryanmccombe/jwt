724f17a873784126239f7c70ec970da2bdf595be
Ryan <ryanmccombe@gmail.com>
2015-02-20 20:00:13 +0000

    Gruntfile

--- front_end/Gruntfile.js
+++ front_end/Gruntfile.js
@@ -394,7 +394,10 @@ module.exports = function (grunt) {
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
+      return grunt.task.run([
+        // 'build',
+        'connect:dist:keepalive'
+      ]);
     }
 
     grunt.task.run([
